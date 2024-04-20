1201e901747b3c1b4fa3222020828263f080a3ae
Kirill <vologdinkirill48@gmail.com>
2024-04-20 21:23:58 +0600

    Add bin-int directory to gitignore; Add premake5.lua;

---
+++ premake5.lua
@@ -0,0 +1,37 @@
+project "assimp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"code/**.h",
+		"code/**.cpp",
+		"code/**.hpp",
+		"code/**.c",
+		"code/**.inl"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "on"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
