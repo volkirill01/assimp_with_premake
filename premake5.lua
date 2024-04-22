2581c1bfc3f1db83b6cc710284d20d428080bca0
Kirill <vologdinkirill48@gmail.com>
2024-04-22 14:34:12 +0600

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -2,38 +2,70 @@ project "assimp"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++17"
-    staticruntime "off"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
-	{
-		"include/**.h",
-		"include/**.hpp",
-		"include/**.inl",
-		"code/**.h",
-		"code/**.cpp",
-		"code/**.hpp",
-		"code/**.c",
-		"code/**.inl",
+	defines {
+	 	"_CRT_SECURE_NO_WARNINGS",
 
-		"contrib/**.h",
-		"contrib/**.cpp",
-		"contrib/**.hpp",
-		"contrib/**.c",
-		"contrib/**.inl",
+		"ASSIMP_BUILD_NO_C4D_IMPORTER",
+		"ASSIMP_BUILD_NO_COMPRESSED_IFC",
+		"ASSIMP_BUILD_NO_IFC_IMPORTER",
+		"ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+		"ASSIMP_BUILD_NO_OPENGEX_EXPORTER",
+		"ASSIMP_BUILD_NO_GLTF_EXPORTER",
+		"ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+		"ASSIMP_BUILD_NO_EXPORT",
+
+		"N_FSEEKO"
 	}
 
-	includedirs
-	{
+	files {
+		"include/**.h",
+		"include/**.cpp",
+		"include/**.cpp",
+		"code/**.h",
+		"code/**.cpp",
+		"contrib/poly2tri/poly2tri/**.h",
+		"contrib/poly2tri/poly2tri/**.cc",
+		"contrib/pugixml/src/**.cpp",
+		"contrib/pugixml/src/**.hpp",
+		"contrib/rapidjson/include/rapidjson/**.h",
+		"contrib/stb/**.h",
+		"contrib/unzip/**.h",
+		"contrib/unzip/**.c",
+		"contrib/utf8cpp/source/**/h",
+		"contrib/zip/src/**.h",
+		"contrib/zip/src/**.c",
+		"contrib/zlib/**.h",
+		"contrib/zlib/**.c"
+	}
+
+	includedirs {
+		"$(ProjectDir)",
 		"include",
 		"code",
-		"contrib/**"
+		"code/Common",
+		"contrib",
+		"contrib/zlib",
+	 	"contrib/zip/src",
+	 	"contrib/unzip",
+		"contrib/pugixml/src",
+		"contrib/utf8cpp/source",
+		"contrib/rapidjson/include"
 	}
 
 	filter "system:windows"
 		systemversion "latest"
+		defines {
+			"_WINDOWS"
+		}
+	filter "system:not windows"
+		defines {
+			'HAVE_UNISTD_H'
+		}
 
 	filter "system:linux"
 		pic "on"
@@ -47,7 +79,7 @@ project "assimp"
 		runtime "Release"
 		optimize "on"
 
-    filter "configurations:Dist"
+	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
-        symbols "off"
+		symbols "off"
